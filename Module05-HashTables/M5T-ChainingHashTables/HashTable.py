67b9d0c8fe0ba73fdb21e8944e3443471262c4c4
MrChiff <105020263+MrChiff@users.noreply.github.com>
2023-03-21 20:12:40 -0400

    Added the files from CH 5.10 to M5T

---
+++ Module05-HashTables/M5T-ChainingHashTables/HashTable.py
@@ -0,0 +1,26 @@
+# CSC 249 Module 5 - Hash Tables
+# M5T - Chaining Hash Table
+# Jonathan Hardwick
+# 2023/03/26
+
+# operations.
+class HashTable:
+    # Returns a non-negative hash code for the specified key.
+    def hashKey(self, key):
+        return abs(hash(key))
+    
+    # Inserts the specified key/value pair. If the key already exists, the 
+    # corresponding value is updated. If inserted or updated, True is returned. 
+    # If not inserted, then False is returned.
+    def insert(self, key, value):
+        pass
+    
+    # Searches for the specified key. If found, the key/value pair is removed 
+    # from the hash table and True is returned. If not found, False is returned.
+    def remove(self, key):
+        pass
+    
+    # Searches for the key, returning the corresponding value if found, None if 
+    # not found.
+    def search(self, key):
+        pass
